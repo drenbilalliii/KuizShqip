fbac63952e087bd967ebad04d1a7ae48e839321e
gramosshala <77172595+gramosshala@users.noreply.github.com>
2021-01-19 00:06:39 +0100

    PerdoruesiException

---
+++ src/main/java/com/example/demo/exceptions/PerdoruesiException.java
@@ -0,0 +1,14 @@
+package com.example.demo.exceptions;
+
+import com.example.demo.model.PerdoruesiEntity;
+
+/**
+ * author Gramos Shala
+ */
+
+public class PerdoruesiException extends Exception {
+
+    public PerdoruesiException(String msg){
+        super(msg);
+    }
+}
